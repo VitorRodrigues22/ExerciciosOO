f89ee00dddae6dc2fb462b52c07963c3fc2b63bf
VitorRodrigues <42284677+VitorRodrigues22@users.noreply.github.com>
2020-07-21 09:55:48 -0300

    Create Exercicio06.java

---
+++ src/Exercicio06/Exercicio06.java
@@ -0,0 +1,11 @@
+package Ex_06;
+
+public class Exercicio06 {
+	public static void main(String[] args) {
+		System.out.println("Encapsulamento é uma forma de esconder o funcionamento dos métodos de uma classe, assim se torna visível \r\n" + 
+				"apenas os conceitos necessário para as demais classes manipular esses métodos.\n");
+		
+		System.out.println("Polimorfismo é uma forma de podermos ter acesso as propriedades de uma classe mãe com a superclasse, \r\n" + 
+				"uma ou mais classes podem ter acesso e pode invocar seus métodos e atributos.");
+	}
+}
